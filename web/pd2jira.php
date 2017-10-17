223012eff767df3073ffcc4aa5cd4402b6b19098
borgified <borgified@gmail.com>
2017-10-17 13:03:25 -0700

    remove newlines from summary
    fixes this issue:
    A JIRA ticket failed to be created.
    {"errorMessages":[],"errors":{"summary":"The summary is invalid because
    it contains newline characters."}}

--- web/pd2jira.php
+++ web/pd2jira.php
@@ -35,6 +35,7 @@ if ($messages) foreach ($messages->messages as $webhook) {
       }
 
       $summary = "PagerDuty Service: $service_name, Incident #$incident_number, Summary: $trigger_summary_data";
+      $summary = trim(preg_replace('/\s+/', ' ', $summary));
       $summary = (strlen($summary) > 255) ? substr($summary,0,252) . '...' : $summary;
 
       $verb = "triggered";
